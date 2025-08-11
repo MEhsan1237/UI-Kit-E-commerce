762ebe2c8a6c24eea32f758e955dcff0060e9af9
Muhammad Ehsan <mehsan4270@gmail.com>
2025-08-11 19:16:50 +0500

    details screen changes upload

--- lib/screens/detailsproduct.dart
+++ lib/screens/detailsproduct.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ProductDetailPage extends StatefulWidget {
   const ProductDetailPage({super.key});
@@ -9,10 +10,10 @@ class ProductDetailPage extends StatefulWidget {
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
   final List<String> images = [
-    'assets/images/image_popular_product_1.png',
-    'assets/images/image_popular_product_2.png',
-    'assets/images/ps4_console_blue_1.png',
     'assets/images/ps4_console_white_1.png',
+    'assets/images/ps4_console_white_2.png',
+    'assets/images/ps4_console_white_3.png',
+    'assets/images/ps4_console_blue_1.png',
   ];
 
   int selectedIndex = 0;
@@ -23,19 +24,33 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
   final List<Color> productColors = [
     Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.yellow
+    Colors.purple,
+    Colors.grey,
+    Colors.lightGreen
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Product Details"),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "images/icons/back_icon.svg",
+            width: 20,
+            height: 20,
+          ),
+        ),
+
+        actions: [
+         Text("4.7"),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 14.0,left: 3),
+            child: SvgPicture.asset("assets/icons/star_icon.svg"),
+          ),
+
+        ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
@@ -50,7 +65,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
       children: [
         // Smaller main image
         SizedBox(
-          height: 180,
+          height: 170,
           child: Center(
             child: Image.asset(
               images[selectedIndex],
@@ -62,71 +77,79 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
         const SizedBox(height: 12),
 
         // Thumbnails Row
-        SizedBox(
-          height: 70,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemCount: images.length,
-            separatorBuilder: (_, __) => const SizedBox(width: 10),
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: selectedIndex == index
-                          ? Colors.blue
-                          : Colors.transparent,
-                      width: 2,
+        Padding(
+          padding: const EdgeInsets.only(left: 40.0),
+          child: SizedBox(
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: images.length,
+              separatorBuilder: (_, __) => const SizedBox(width: 30),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: selectedIndex == index
+                            ? Colors.blue
+                            : Colors.transparent,
+
+
+                      ),
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.asset(
-                      images[index],
-                      width: 70,
-                      height: 70,
-                      fit: BoxFit.cover,
+                    child: ClipRRect(
+
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        images[index],
+                        width: 40,
+                        height: 1,
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
 
-        const SizedBox(height: 16),
+        const SizedBox(height: 36),
 
         // Product Name + Favorite icon (separate lines)
         const Text(
-          "Product Name",
+         "Wireless Controller for PS4™",
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
           ),
         ),
-        IconButton(
-          icon: Icon(
-            isFavorite ? Icons.favorite : Icons.favorite_border,
-            color: Colors.red,
+        Align( alignment: Alignment.centerRight,
+          child: IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Colors.red,
+            ),
+            onPressed: () {
+              setState(() {
+                isFavorite = !isFavorite;
+              });
+            },
           ),
-          onPressed: () {
-            setState(() {
-              isFavorite = !isFavorite;
-            });
-          },
         ),
 
         // Short or Full Description
         Text(
           isDescriptionExpanded
-              ? "This is the full description of the product. Here you can see all the details about the product, its features, material, size, and any other information you want to add."
-              : "This is a short description of the product...",
+              ? "with other through the laptops and the mobile phones."
+              : "Wireless Controller for PS4™ gives you what you want in your gaming from over precision your games to sharing...",
           style: const TextStyle(fontSize: 14, color: Colors.black54),
         ),
 
@@ -203,16 +226,18 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
         // Add to Cart Button
         SizedBox(
+          height: 40,
           width: double.infinity,
           child: ElevatedButton(
+
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(content: Text("Product added to cart")),
               );
             },
             style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 14),
-              backgroundColor: Colors.blue,
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              backgroundColor: Colors.deepOrangeAccent,
             ),
             child: const Text(
               "Add to Cart",
